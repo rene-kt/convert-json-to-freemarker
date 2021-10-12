986f5ce4873485806a09a3fdc8418d5ac9a71c6f
Rene <renejr.arraes286@gmail.com>
2021-10-12 09:21:39 -0300

    create the kdoc

--- src/main/kotlin/jackson/ConvertJsonToObject.kt
+++ src/main/kotlin/jackson/ConvertJsonToObject.kt
@@ -1,6 +1,5 @@
 package jackson
 
-import com.fasterxml.jackson.databind.DeserializationFeature
 import com.fasterxml.jackson.module.kotlin.jacksonObjectMapper
 import com.fasterxml.jackson.module.kotlin.readValue
 import models.Screen
@@ -12,6 +11,11 @@ class ConvertJsonToObject {
     "application": {
         "screens": ["""
 
+    /**
+     * Get a given JSON from KnowCode API and converts to a Screen object
+     *
+     * @param json the JSON from the API.
+     */
     fun convert(json: String): Screen {
         return mapper.readValue<Screen>(json.replace(jsonIgnore, ""))
     }
